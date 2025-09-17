b560da56f6abc21cd2b6e6066b62a60bb4abbf83
Kaleeshwari3054 <kaleeshwari894@gmail.com>
2025-09-17 21:12:57 +0530

    fix netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0',   // allows LAN access too
+//     port: 5173,        // ensure it always uses 5173
+//     strictPort: true,  // fail if 5173 is busy instead of changing port
+//     hmr: {
+//       protocol: 'ws',  // or 'wss' if using HTTPS
+//       host: 'localhost',
+//       port: 5173
+//     }
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',   // allows LAN access too
-    port: 5173,        // ensure it always uses 5173
-    strictPort: true,  // fail if 5173 is busy instead of changing port
-    hmr: {
-      protocol: 'ws',  // or 'wss' if using HTTPS
-      host: 'localhost',
-      port: 5173
-    }
-  }
+  base: "/",  // 👈 important for correct paths on Netlify
 })
