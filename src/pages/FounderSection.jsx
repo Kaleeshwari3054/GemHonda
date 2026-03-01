f4b5be03ad4e4038b4bba91b92c85c6686e0868b
Kaleeshwari <https://github.com/Kaleeshwari3054/GemHonda>
2026-03-01 21:15:01 +0530

    Founder section update logo

--- src/pages/FounderSection.jsx
+++ src/pages/FounderSection.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import "../Styles/FounderSection.css";
-
-
 // import Logo from "../assets/Logo.jpg";
-
-// import Logo from "../assets/GemHondaLogo.png";
 import Logo from "../assets/GemHondaLogo.png";
 const FounderSection = () => {
   return (
