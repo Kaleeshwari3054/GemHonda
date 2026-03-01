124880999246c7f0bb363fddc516658847fb082c
Kaleeshwari <https://github.com/Kaleeshwari3054/GemHonda>
2026-03-01 14:02:02 +0530

    update logo recent

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -101,7 +101,7 @@
 import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import "../Styles/Navbar.css";
-// import Logo from "../assets/GemHondaLogo.png";
+// import Logo from "../assets/GemHondaLogo.png";;
 import Logo from "../assets/Home-logo.png"
 
 const Navbar = () => {
